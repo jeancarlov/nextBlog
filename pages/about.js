1dc633d68f13cf990b2f38260d694f81eef92dde
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2020-03-10 19:01:53 -0400

    first commit with basic setup for blog app

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/MyLayout';
+
+function about() {
+    return (
+    <Layout>
+        <p>This is the about page</p>
+    </Layout>
+    )
+}
+
+export default about
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Link from 'next/link';
+// import Header from "../components/Header";
+import Layout from '../components/MyLayout';
+
+function index() {
+    return (
+        <Layout>
+        <p>Hello Next.js</p>
+      </Layout>
+    );
+}
+
+export default index

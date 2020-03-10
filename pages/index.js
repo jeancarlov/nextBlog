8e0ffbc50748a4eb5b1f4e3f1d85a544372cb648
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2020-03-10 19:29:28 -0400

    router.query added to display the title from query strings

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
-// import Link from 'next/link';
+import Link from 'next/link';
 // import Header from "../components/Header";
 import Layout from '../components/MyLayout';
 
-function index() {
-    return (
-        <Layout>
-        <p>Hello Next.js</p>
-      </Layout>
-    );
-}
+// function index() {
+//     return (
+//         <Layout>
+//         <p>Hello Next.js</p>
+//       </Layout>
+//     );
+// }
 
-export default index
+// export default index
+
+
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Learn Next.js is awesome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
+    </Layout>
+  );
+}

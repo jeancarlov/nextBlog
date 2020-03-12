2a097f881ad35be254be5f82bd585b902b3c4538
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2020-03-12 19:35:24 -0400

    added dependency to fetch data, data is fetching succesfully

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Link from 'next/link';
-// import Header from "../components/Header";
 import Layout from '../components/MyLayout';
-import Post from '../pages/p/[id].js';
+import fetch from 'isomorphic-unfetch';
+// import Header from "../components/Header";
+
+// import Post from '../pages/p/[id].js';
 
 // function index() {
 //     return (
@@ -15,22 +17,51 @@ import Post from '../pages/p/[id].js';
 // export default index
 
 
-const PostLink = props => (
-  <li>
-    <Link href="/p/[id]" as={`/p/${props.id}`}>
-    <a>{props.id}</a>
-    </Link>
-  </li>
+// const PostLink = props => (
+//   <li>
+//     <Link href="/p/[id]" as={`/p/${props.id}`}>
+//     <a>{props.id}</a>
+//     </Link>
+//   </li>
+// );
+// export default function Blog() {
+//   return (
+//     <Layout>
+//       <h1>My Blog</h1>
+//       <ul>
+//         <PostLink title="Hello Next.js" />
+//         <PostLink title="Learn Next.js is awesome" />
+//         <PostLink title="Deploy apps with Zeit" />
+//       </ul>
+//     </Layout>
+//   );
+// }
+
+const Index = props => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
 );
-export default function Blog() {
-  return (
-    <Layout>
-      <h1>My Blog</h1>
-      <ul>
-        <PostLink title="Hello Next.js" />
-        <PostLink title="Learn Next.js is awesome" />
-        <PostLink title="Deploy apps with Zeit" />
-      </ul>
-    </Layout>
-  );
-}
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
+
+export default Index;
+

4a1a98d4dee0ca3efd76c176ba639f8562ed895a
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2020-03-12 19:46:20 -0400

    more data fetching with more content before styling

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,13 +1,35 @@
 import { useRouter } from 'next/router';
 import Layout from '../../components/MyLayout';
 
-export default function Post() {
-  const router = useRouter();
+// export default function Post() {
+//   const router = useRouter();
 
-  return (
+//   return (
+//     <Layout>
+//       <h1>{router.query.id}</h1>
+//       <p>This is the blog post content test.</p>
+//     </Layout>
+//   );
+// }
+
+const Post = props => (
     <Layout>
-      <h1>{router.query.id}</h1>
-      <p>This is the blog post content test.</p>
+      <h1>{props.show.name}</h1>
+      <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+      {props.show.image ? <img src={props.show.image.medium} /> : null}
     </Layout>
   );
-}
+  
+  Post.getInitialProps = async function(context) {
+    const { id } = context.query;
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+    const show = await res.json();
+    // console.log(show);
+  
+    console.log(`Fetched show: ${show.name}`);
+  
+    return { show };
+  };
+  
+  export default Post;
+

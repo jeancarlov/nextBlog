2acbb7e8c4b8bde66516bd4b6f6e25a07af4bd73
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2020-03-13 20:42:52 -0400

    removed style-jsx and change component name into Navbar

--- components/Header.js
+++ components/Header.js
@@ -17,41 +17,66 @@ import Link from 'next/link';
 
 
 
-const Header = () => (
-  <div> 
-    <ul>
-      <li> <Link href="/">
-      <a>Home</a>
-    </Link>
-      </li>
-      <li><Link href="/about">
-      <a>About</a>
-    </Link>
-      </li>
-    </ul>
+// const Header = () => (
+//   <div> 
+//     <ul>
+//       <li> <Link href="/">
+//       <a>Home</a>
+//     </Link>
+//       </li>
+//       <li><Link href="/about">
+//       <a>About</a>
+//     </Link>
+//       </li>
+//     </ul>
 
-    <style jsx>{
-     `
-     ul {
-       background: #333;
-       color: #fff;
-       list-style: none;
-       display: flex;
-     }
+//     <style jsx>{
+//      `
+//      ul {
+//        background: #333;
+//        color: #fff;
+//        list-style: none;
+//        display: flex;
+//      }
      
-     ul li {
-       font-size: 18px;
-       margin-right: 20px;
-     }
+//      ul li {
+//        font-size: 18px;
+//        margin-right: 20px;
+//      }
 
-     ul li a{
-       color: #fff;
-       text-decoration: none;
-     }
-     `
-    }</style>
+//      ul li a{
+//        color: #fff;
+//        text-decoration: none;
+//      }
+//      `
+//     }</style>
    
     
+//   </div>
+// );
+// export default Header;
+
+// Note : header works as the navbar 
+
+const Navbar = () => (
+  <div> 
+   
+
+    <nav className="navbar navbar-expand navbar-dark bg-dark mb-4">
+    <div className="container">
+      <a className="navbar-brand" href="/">100% Hustle</a>
+      <div className="collapse navbar-collapse">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link href="/"><a className="nav-link">Home</a></Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/about"><a className="nav-link">About</a></Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   </div>
 );
-export default Header;
+export default Navbar;
